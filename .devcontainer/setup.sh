b98dbf5429d7157e7bacb885150cf9fba5ff4d61
Rajat Batra <rajat1691@hotmail.com>
2024-02-21 21:22:59 +0530

    Adding Docker file  for github codespaces
    Signed-off-by: Rajat Batra <rajat1691@hotmail.com>

---
+++ .devcontainer/setup.sh
@@ -0,0 +1,15 @@
+## update and install some things we should probably have
+apt-get update
+apt-get install -y \
+  curl \
+  git \
+  gnupg2 \
+  jq \
+  sudo \
+  zsh \
+  vim \
+  build-essential \
+  openssl
+
+## Install rustup and common components
+curl https://sh.rustup.rs -sSf | sh -s -- -y 
